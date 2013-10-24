f158d13836d875901e013b983f921c1c53b5501d
Shilad Sen <shilad@gmail.com>
2013-10-24 00:27:22 -0500

    Added new files.

--- make.sh
+++ make.sh
@@ -1,3 +1,12 @@
-rm -rf published/* &&
+rm -f .build/static &&
+rm -rf published &&
+mkdir published &&
 cp -rp .build/* published/ &&
-cp -rp static/* published/
+cp -rp static/ published/
+
+if [ -d /var/www/html ]; then
+	sudo rm -rf /var/www/html/www.shilad.com &&
+	sudo mkdir /var/www/html/www.shilad.com &&
+	sudo cp -rp published/* /var/www/html/www.shilad.com/ &&
+	sudo chmod -R 777 /var/www/html/www.shilad.com/
+fi
