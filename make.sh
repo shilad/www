186642c97a6d9d4ddf0863f2ad80f42a813e6531
Shilad Sen <shilad@gmail.com>
2015-02-20 17:11:41 -0600

    Updates to website.

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
+cactus build &&
 rm -f .build/static &&
 rm -rf published &&
 mkdir published &&
@@ -5,8 +6,8 @@ cp -rp .build/* published/ &&
 cp -rp static/ published/
 
 if [ -d /var/www/html ]; then
-	sudo rm -rf /var/www/html/www.shilad.com &&
-	sudo mkdir /var/www/html/www.shilad.com &&
+	#sudo rm -rf /var/www/html/www.shilad.com &&
+	#sudo mkdir /var/www/html/www.shilad.com &&
 	sudo cp -rp published/* /var/www/html/www.shilad.com/ &&
 	sudo chmod -R 777 /var/www/html/www.shilad.com/
 fi
